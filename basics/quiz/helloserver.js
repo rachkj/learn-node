b6f1f65ab989be0178f8d7d5e1a22e884dedef33
Rachana Kallada Jayaraj <kjrach@gmail.com>
2024-02-26 18:36:21 -0500

    Added code

--- basics/quiz/helloserver.js
+++ basics/quiz/helloserver.js
@@ -1,27 +1,31 @@
 #! /opt/homebrew/bin/node
-const http = require('http')
+const http = require('http');
+const os = require('os');
 const args = process.argv;
 const getConfig = require('../config');
-
 
 const config = getConfig(args);
 const hostname = config.hostname;
 const port = config.port;
 
 const server = http.createServer((req, res) => {
-    if(req.url && req.url.endsWith('/home')) {
+    if (req.url && req.url.endsWith('/home')) {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.end('Welcome to home!');
+    } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+
+        const responseObj = {
+            hostname: os.hostname(),
+            port: port,
+        };
+
+        res.end(JSON.stringify(responseObj));
     }
-    else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Invalid request due to bad URL');
-    }
-    
-})
+});
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-})
+});
--- operations/quiz/syncdelete.js
+++ operations/quiz/syncdelete.js
@@ -1,7 +1,12 @@
 const fs = require("fs");
-fs.readFile("./operations/data.txt", (err, data) => {
-  if (err) console.log(err.message);
-  else console.log(data);
-});
-fs.unlinkSync("./operations/data.txt");		// delete the file
 
+fs.readFile("./operations/data.txt", (err, data) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(data);
+
+    // Move the file deletion operation inside the callback
+    fs.unlinkSync("./operations/data.txt"); // delete the file
+  }
+});
